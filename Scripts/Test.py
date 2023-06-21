6ccddb3cb831477538d87b647db2817edf5076c9
HarshVanasiwala-18 <harshvanasiwala18@gmail.com>
2023-06-20 22:08:51 -0400

    Done with IOC scraper & Malpedia yara and articles

--- Scripts/Test.py
+++ Scripts/Test.py
@@ -1,27 +1,17 @@
-import json
 import requests
 from bs4 import BeautifulSoup
 
-def scarpe_ioc_pulses():
-    with open("CTI DB\IOCs\pulses_id.txt", 'r') as file:
-        lines = file.read().splitlines() 
-        for line in lines:
-            j = 1
-            while True:
-                try:
-                    response = requests.get(f'https://otx.alienvault.com/otxapi/pulses/{line}/indicators/?sort=-created&limit=100000000&page={j}')
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    json_data = json.loads(soup.prettify())
-                    j += 1
-                    if json_data['results'] != []:
-                        for i in range(len(json_data['results'])):
-                            print(json_data['results'][i]['indicator'], json_data['results'][i]['type'])
-                    elif json_data['results'] == []:
-                        break
-                except Exception as e:
-                    print("Exception: ", e)
-                    break
+resp = requests.get("https://malpedia.caad.fkie.fraunhofer.de/details/win.404keylogger")
+soup = BeautifulSoup(resp.text, "html.parser")
 
+yara_signature_found = False
 
-if __name__ == "__main__":
-    scarpe_ioc_pulses()
+for row in soup.find_all("pre"):
+    text = row.text
+    if not text:
+        print("No Yara Signature Found")
+        yara_signature_found = True
+        break
+
+if not yara_signature_found:
+    print("Yara Signature Found")
