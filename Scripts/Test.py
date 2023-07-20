4feec1c3404589c408a5a039a71cfae95e9d4cd0
HarshVanasiwala-18 <harshvanasiwala18@gmail.com>
2023-07-19 22:15:35 -0400

    Regex code added check test.py

--- Scripts/Test.py
+++ Scripts/Test.py
@@ -1,17 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+import re
 
-resp = requests.get("https://malpedia.caad.fkie.fraunhofer.de/details/win.404keylogger")
-soup = BeautifulSoup(resp.text, "html.parser")
+regex_ipv6 = r'([a-f0-9:]+:+)+[a-f0-9]+'
+# regex_ipv4 = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+# regex_filename = r'[A-Za-z0-9-_\·]+\.(txt|php|exe|dll|bat|sys|htm|html|js|jar|jpg|png|vb|scr|pif|chm|zip|rar|cab|pdf|doc|docx|ppt|pptx|xls|xlsx|swf|gif|bat|pdf)'
+regex_filepath = r'[a-z A-Z]:(\\([0-9 a-z A-Z _]+))+'
+regex_sha1 = r'[a-f0-9]{40}|[A-F0-9]{40}'
+regex_sha256 = r'[a-f0-9]{64}|[A-F0-9]{64}'
+regex_sha512 = r'[a-f0-9]{128}|[A-F0-9]{128}'
+regex_md5 = r'[a-f0-9]{32}|[A-F0-9]{32}'
+regex_cve = r'CVE-[0-9]{4}-[0-9]{4,6}'
+regex_domain = r'[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\[\.\]|\{\.\}|\(\.\)|\.)[a-zA-Z]{2,6}'
+regex_url = r'(https?|ftp|file|http)://[-A-Za-z0-9+&@#/%?=∼ _|! : , .;]+[-A-Za-z0-9+&@#/%?=∼ _|]'
+regex_email = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
+regex_filename= r'\b([a-zA-Z0-9_-]+)(?:\[\.\]|\{\.\}|\(\.\)|\.)([a-zA-Z0-9]+)\b'
+regex_ipv4 = r'\d{1,3}(?:\[\.\]|\{\.\}|\(\.\)|\.)\d{1,3}(?:\[\.\]|\{\.\}|\(\.\)|\.)\d{1,3}(?:\[\.\]|\{\.\}|\(\.\)|\.)\d{1,3}'
 
-yara_signature_found = False
-
-for row in soup.find_all("pre"):
-    text = row.text
-    if not text:
-        print("No Yara Signature Found")
-        yara_signature_found = True
-        break
-
-if not yara_signature_found:
-    print("Yara Signature Found")
